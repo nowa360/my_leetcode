fec682efe7b35a55a6c7e086ad49fb6e59f9a616
nowa360 <noahsung1226@hotmail.com>
2020-06-09 22:48:17 -0400

    RandomizedSet

---
+++ Others/RandomizedSet.py
@@ -0,0 +1,47 @@
+from random import random
+
+
+class RandomizedSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.nums = []
+        self.pos = {}
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.pos:
+            self.nums.append(val)
+            self.pos[val] = len(self.nums) - 1
+            return True
+        return False
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.pos:
+            idx = self.pos[val]
+            last_val = self.nums[-1]
+            self.nums[idx] = last_val
+            self.pos[last_val] = idx
+            del self.pos[val]
+            self.nums.pop()
+            return True
+        return False
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        rand = random.randint(0, len(self.nums) - 1)
+        return self.nums[rand]
