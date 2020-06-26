8229ecdf67e0c6965015fca496dedb4c25699cf0
nowa360 <noahsung1226@hotmail.com>
2020-06-25 21:40:32 -0400

    findDuplicate

---
+++ JuneChallenge/FindDuplicate.py
@@ -0,0 +1,43 @@
+"""
+June 25 Challenge - Find the Duplicate Number
+
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive),
+prove that at least one duplicate number must exist.
+Assume that there is only one duplicate number, find the duplicate one.
+
+Example 1:
+
+Input: [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: [3,1,3,4,2]
+Output: 3
+Note:
+
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
+"""
+
+
+def findDuplicate(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    if len(nums) > 1:
+        slow = nums[0]
+        fast = nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        fast = 0
+        while fast != slow:
+            fast = nums[fast]
+            slow = nums[slow]
+        return slow
+
+    return -1
