efe79fb2408b8a7ed6756a03dd38162ea57bb3aa
nowa360 <noahsung1226@hotmail.com>
2020-08-18 10:13:49 -0400

    Distribute Candies to People

---
+++ AugustChallenge/DistributeCandiesToPeople.py
@@ -0,0 +1,38 @@
+"""
+August 17 Challenge - Distribute Candies to People
+
+Solution
+We distribute some number of candies, to a row of n = num_people people in the following way:
+
+We then give 1 candy to the first person, 2 candies to the second person, and so on until we give n candies to the
+last person.
+
+Then, we go back to the start of the row, giving n + 1 candies to the first person, n + 2 candies to the second person,
+and so on until we give 2 * n candies to the last person.
+
+This process repeats (with us giving one more candy each time, and moving to the start of the row after
+we reach the end) until we run out of candies.
+
+The last person will receive all of our remaining candies (not necessarily one more than the previous gift).
+
+Return an array (of length num_people and sum candies) that represents the final distribution of candies.
+"""
+
+
+def distributeCandies(candies, num_people):
+    """
+    :type candies: int
+    :type num_people: int
+    :rtype: List[int]
+    """
+    ppl = [0] * num_people
+    required, curr = 1, 0
+
+    while candies >= required:
+        ppl[curr % num_people] += required
+        curr += 1
+        candies -= required
+        required += 1
+
+    ppl[curr % num_people] += candies
+    return ppl
