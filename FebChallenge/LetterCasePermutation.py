e07c7f821ca6a6d1087f2a7106fa6e1b7ab97186
nowa360 <noahsung1226@hotmail.com>
2021-02-16 22:51:09 -0500

    Letter Case Permutation

---
+++ FebChallenge/LetterCasePermutation.py
@@ -0,0 +1,54 @@
+"""
+Feb 16 Challenge - Letter Case Permutation
+
+Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.
+
+Return a list of all possible strings we could create. You can return the output in any order.
+
+
+
+Example 1:
+
+Input: S = "a1b2"
+Output: ["a1b2","a1B2","A1b2","A1B2"]
+Example 2:
+
+Input: S = "3z4"
+Output: ["3z4","3Z4"]
+Example 3:
+
+Input: S = "12345"
+Output: ["12345"]
+Example 4:
+
+Input: S = "0"
+Output: ["0"]
+
+
+Constraints:
+
+S will be a string with length between 1 and 12.
+S will consist only of letters or digits.
+"""
+
+
+def letterCasePermutation(S):
+    """
+    :type S: str
+    :rtype: List[str]
+    """
+
+    res = []
+
+    def letter_helper(curr_word, idx):
+        if idx == len(S):
+            res.append(curr_word)
+        elif S[idx].isalpha():
+            letter_helper(curr_word + S[idx].upper(), idx + 1)
+            letter_helper(curr_word + S[idx].lower(), idx + 1)
+        else:
+            letter_helper(curr_word + S[idx], idx + 1)
+
+    letter_helper("", 0)
+
+    return res
